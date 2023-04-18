b0bbf0b16acc13d2c63b74812202beda4ec004d5
lya-melnik <ilay.melnik.kr@gmail.com>
2023-04-18 16:05:46 -0700

    NavLink, Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*// NavLink is link, to={create url}*/}
+            <NavLink to={'/'}>main</NavLink> --
+            <NavLink to={'/login'}>login</NavLink> --
+            <NavLink to={'/profile'}>profile</NavLink>
+
+
+            {/*читает путь и при совпадении path and url, отрисовывает element (может быть и компонента) */}
+            <Routes>
+                <Route path={'/'} element={<div>main</div>}></Route>
+                <Route path={'/login'} element={<div>login</div>}></Route>
+                <Route path={'/profile'} element={<div>profile</div>}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
